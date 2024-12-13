80875bef247f8f94f5ffbb7b94d620c64100618b
Anand Dua <ananddua5@gmail.com>
2024-12-13 20:22:14 +0530

    added new endpoint called notes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const express =  require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("anand dua is wrtting these end points");
 });
 
-const port = 3000;
+
+app.get('/notes', (req, res) => {
+    res.send("add git ignore  \n   make better functions");
+});
 
 app.listen(port, () => {
     console.log("app is listening to port 3000");
-})
+});
